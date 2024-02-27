2bdffe9a64c1dcccc6a2611e1e24339a394a72c7
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-02-27 20:25:00 +0000

    Add just, bat, and btop

--- home.nix
+++ home.nix
@@ -24,18 +24,7 @@ in {
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    just
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -57,6 +46,53 @@ in {
 
   programs = {
     home-manager.enable = true;
+
+    bat = {
+      enable = true;
+      config = {
+        theme = "OneHalfDark";
+        italic-text = "always";
+      };
+      syntaxes = {
+        just = {
+          src = pkgs.fetchFromGitHub {
+            owner = "nk9";
+            repo = "just_sublime";
+            rev = "352bae277961d41e2a1795a834dbf22661c8910f";
+            hash = "sha256-QCp6ypSBhgGZG4T7fNiFfCgZIVJoDSoJBkpcdw3aiuQ=";
+          };
+          file = "Syntax/Just.sublime-syntax";
+        };
+        ron = {
+          src = pkgs.fetchFromGitHub {
+            owner = "ron-rs";
+            repo = "sublime-ron";
+            rev = "41e3e37db9febbf9dfc4efad015934c61b3c8ef1";
+            hash = "sha256-t7ILpYizGIoICKe3NzBkeb9fzou0d7NkyDjLIUsa9KE=";
+          };
+          file = "RON.sublime-syntax";
+        };
+        wolfram = {
+          src = pkgs.fetchFromGitHub {
+            owner = "WolframResearch";
+            repo = "Sublime-WolframLanguage";
+            rev = "1d2da4b347a03d07e1b816a25a02c1a992230d3a";
+            hash = "sha256-9MaD96xBAHF4ubuvoqMz3uJGUHE36lIiuOjJkAAucTI=";
+          };
+          file = "WolframLanguage.sublime-syntax";
+        };
+      };
+    };
+
+    btop = {
+      enable = true;
+      settings = {
+        theme = "onedark";
+        theme_background = false;
+        update_ms = 500;
+        proc_tree = true;
+      };
+    };
 
     neovim = {
       enable = true;
