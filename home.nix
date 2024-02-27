1c41cf041d4e6c928beb0ad486783adf90058601
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-02-27 15:05:40 +0000

    Add git config

--- home.nix
+++ home.nix
@@ -306,5 +306,69 @@ in {
         }
       '';
     };
+
+    git = {
+      enable = true;
+      userName = "DoctorDalek1963";
+      userEmail = "dyson.dyson@icloud.com";
+      aliases = {
+        a = "add";
+        aa = "add -A";
+        ae = "add -e";
+        aea = "add -e -A";
+        ap = "add -p";
+        apa = "add -p -A";
+        br = "branch";
+        c = "commit";
+        ca = "commit -a";
+        cam = "commit -a -m";
+        cane = "commit --amend --no-edit";
+        cm = "commit -m";
+        co = "checkout";
+        cob = "checkout -b";
+        fixm = "commit --amend";
+        l = "log";
+        la = "log --all --decorate";
+        last = "log -1 HEAD";
+        lg = "log --all --graph --oneline --decorate --notes --date-order";
+        lo = "log --oneline";
+        pfwl = "push --force-with-lease";
+        puom = "push -u origin main";
+        sh = "show";
+        st = "status";
+        sw = "switch";
+        unstage = "restore --staged";
+        uncommit = "reset --soft HEAD~1";
+      };
+      signing = {
+        key = "F1C0D1EBB627CF58";
+        signByDefault = true;
+      };
+      delta = {
+        enable = true;
+        options = {
+          navigate = true;
+          light = false;
+          syntax-theme = "OneHalfDark";
+          line-numbers = true;
+        };
+      };
+      extraConfig = {
+        core.editor = "${pkgs.neovim}/bin/nvim";
+        diff.colorMoved = "default";
+        init.defaultBranch = "main";
+        pull = {
+          rebase = false;
+          ff = "only";
+        };
+        push = {
+          followTags = true;
+          autoSetupRemote = true;
+          default = "current";
+        };
+        fetch.prune = true;
+        merge.ff = false;
+      };
+    };
   };
 }
