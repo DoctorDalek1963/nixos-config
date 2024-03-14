fefde5fd70db027f70d37c9be69fd5a5a6afe4eb
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-03-14 15:36:28 +0000

    Make sops-nix systemd service reloading less platform-specific

--- home-manager/dyson/modules/secrets.nix
+++ home-manager/dyson/modules/secrets.nix
@@ -7,9 +7,7 @@
   imports = [sops-nix.homeManagerModules.sops];
 
   # See https://github.com/Mic92/sops-nix#use-with-home-manager
-  home.activation.setupEtc = config.lib.dag.entryAfter ["writeBoundary"] ''
-    /run/current-system/sw/bin/systemctl start --user sops-nix
-  '';
+  home.activation.setupEtc = config.lib.dag.entryAfter ["writeBoundary"] "systemctl start --user sops-nix";
 
   sops = {
     defaultSopsFile = ./../secrets/secrets.yaml;
