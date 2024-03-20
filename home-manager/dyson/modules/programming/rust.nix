7dcd3d4f14b107c3a6ed68918f2443adfe5f77cf
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-03-20 23:17:56 +0000

    Add evcxr to Rust builds

--- home-manager/dyson/modules/programming/rust.nix
+++ home-manager/dyson/modules/programming/rust.nix
@@ -6,12 +6,17 @@
 }: {
   config = lib.mkIf config.setup.programming.rust {
     home = {
-      packages = with pkgs; [rustup];
+      packages = with pkgs; [
+        evcxr
+        rustup
+      ];
 
       file = {
         ".cargo/clippy.conf".source = ../../files/clippy.conf;
         ".cargo/config.toml".source = ../../files/cargo-config.toml;
       };
     };
+
+    xdg.configFile."evcxr/init.evcxr".text = "use std::mem::{size_of, transmute};";
   };
 }
