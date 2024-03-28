1b6304108539d72b698e2f31e46e93ed06d90521
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-03-28 14:47:14 +0000

    Remove old TODO comment

--- home-manager/dyson/modules/firefox/extensions/default.nix
+++ home-manager/dyson/modules/firefox/extensions/default.nix
@@ -2,37 +2,35 @@
   pkgs,
   config,
 }: let
-  extensions =
-    # TODO: Allow unfree extensions
-    with pkgs.nur.repos.rycee.firefox-addons; [
-      # Privacy
-      duckduckgo-privacy-essentials
-      privacy-badger
-      ublock-origin
+  extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    # Privacy
+    duckduckgo-privacy-essentials
+    privacy-badger
+    ublock-origin
 
-      # Programming
-      github-file-icons
-      refined-github
-      rust-search-extension
-      tampermonkey # Unfree
+    # Programming
+    github-file-icons
+    refined-github
+    rust-search-extension
+    tampermonkey # Unfree
 
-      # YouTube
-      dearrow
-      enhancer-for-youtube # Unfree
-      leechblock-ng
-      return-youtube-dislikes
-      sponsorblock
+    # YouTube
+    dearrow
+    enhancer-for-youtube # Unfree
+    leechblock-ng
+    return-youtube-dislikes
+    sponsorblock
 
-      # Misc
-      darkreader
-      dashlane # Unfree
-      wayback-machine
+    # Misc
+    darkreader
+    dashlane # Unfree
+    wayback-machine
 
-      # Not yet packages
-      # TODO: Package these manually?
-      # Zotero Connector
-      # Who Wrote That?
-    ];
+    # Not yet packages
+    # TODO: Package these manually?
+    # Zotero Connector
+    # Who Wrote That?
+  ];
 in
   if config.setup.firefox.enableExtensions
   then extensions
