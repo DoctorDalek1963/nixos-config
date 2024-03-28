22a15a2cea9b20a823a3306bfd2facfd1c91e7a6
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-03-28 17:05:11 +0000

    Properly link to clang and mold in cargo config

--- home-manager/dyson/modules/programming/rust.nix
+++ home-manager/dyson/modules/programming/rust.nix
@@ -13,7 +13,27 @@
 
       file = {
         ".cargo/clippy.conf".source = ../../files/clippy.conf;
-        ".cargo/config.toml".source = ../../files/cargo-config.toml;
+        ".cargo/config.toml".text = ''
+          [alias]
+          a = "add"
+          b = "build"
+          br = "build --release"
+          c = "check"
+          i = "install"
+          n = "new"
+          r = "run"
+          rr = "run --release"
+          t = "test"
+          tr = "test --release"
+          up = "update"
+
+          [target.x86_64-unknown-linux-gnu]
+          linker = "${pkgs.clang}/bin/clang"
+          rustflags = ["-Clink-arg=-fuse-ld=${pkgs.mold-wrapped}/bin/mold"]
+
+          [target.armv7-unknown-linux-gnueabihf]
+          linker = "arm-linux-gnueabihf-gcc"
+        '';
       };
     };
 
