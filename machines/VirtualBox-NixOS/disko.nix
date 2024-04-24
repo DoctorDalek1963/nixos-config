067d43e2d6391781fb40b07bb1e9ea1b65306320
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-04-24 14:57:15 +0100

    Use LVM in VirtualBox-NixOS

--- machines/VirtualBox-NixOS/disko.nix
+++ machines/VirtualBox-NixOS/disko.nix
@@ -1,29 +1,48 @@
 {
   disko.devices = {
-    disk = {
-      sda = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "512M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+    disk.sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "512M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
+          };
+          primary = {
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
             };
+          };
+        };
+      };
+    };
+
+    lvm_vg.pool = {
+      type = "lvm_vg";
+      lvs = {
+        root = {
+          size = "30G";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+        home = {
+          size = "9G";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/home";
           };
         };
       };
