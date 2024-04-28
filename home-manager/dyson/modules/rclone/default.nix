5d083a1e8e146209a7822a31860c25da6a9a7475
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-04-28 18:56:58 +0100

    Make rclone log outputs

--- home-manager/dyson/modules/rclone/default.nix
+++ home-manager/dyson/modules/rclone/default.nix
@@ -6,6 +6,8 @@
 }: let
   cfg = config.setup.rclone;
 
+  log-dir = "${config.home.homeDirectory}/.rclone-logs";
+
   services = builtins.listToAttrs (builtins.map (opts: {
       name = "rclone-mount-${lib.strings.toLower opts.remote}";
       value = {
@@ -24,8 +26,16 @@
             else "";
         in {
           Type = "simple";
-          ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${opts.mountpoint}";
-          ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode writes ${readonly} ${opts.extraArgs} ${opts.remote}: ${opts.mountpoint}";
+          ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${opts.mountpoint} ${log-dir}";
+          ExecStart = lib.concatStringsSep " " [
+            "${pkgs.rclone}/bin/rclone mount"
+            "--vfs-cache-mode writes"
+            "--log-file \"${log-dir}/${opts.remote}.log\""
+            "--log-level INFO"
+            "${readonly}"
+            "${opts.extraArgs}"
+            "${opts.remote}: ${opts.mountpoint}"
+          ];
           ExecStop = "${pkgs.fuse}/bin/fusermount -u ${opts.mountpoint}";
           Restart = "on-failure";
           RestartSec = "10s";
