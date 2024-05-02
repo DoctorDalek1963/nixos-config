bb5c7e9c11613237993d00e9368809d563cd777b
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-02 14:43:24 +0100

    Disable Wayland when gaming

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -11,5 +11,8 @@
     programs.gamemode.enable = true;
 
     environment.systemPackages = with pkgs; [ludusavi mangohud];
+
+    # Gaming is still broken on Wayland
+    services.xserver.displayManager.gdm.wayland = false;
   };
 }
--- modules/virtualBox/guest.nix
+++ modules/virtualBox/guest.nix
@@ -1,10 +1,14 @@
-{config, ...}: let
-  guest-enabled = config.setup.virtualBox.guest.enable;
-in {
-  virtualisation.virtualbox.guest.enable = guest-enabled;
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.setup.virtualBox.guest.enable {
+    virtualisation.virtualbox.guest.enable = true;
 
-  # I've had some issues with graphics controllers and NixOS in VirtualBox. If
-  # I disable Wayland in the VM and use VBoxVGA, then it seems to work properly
-  # and allow screen resizing.
-  services.xserver.displayManager.gdm.wayland = !guest-enabled;
+    # I've had some issues with graphics controllers and NixOS in VirtualBox. If
+    # I disable Wayland in the VM and use VBoxVGA, then it seems to work properly
+    # and allow screen resizing.
+    services.xserver.displayManager.gdm.wayland = false;
+  };
 }
