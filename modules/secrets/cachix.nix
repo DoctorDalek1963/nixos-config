d132fa59148512d70d9881224f3065e843d7657d
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-08 13:41:12 +0100

    Disable cachix-watch-store

--- modules/secrets/cachix.nix
+++ modules/secrets/cachix.nix
@@ -13,13 +13,6 @@ in {
       "cachix/tokens/doctordalek1963" = {mode = "0400";};
     };
 
-    services.cachix-watch-store = {
-      enable = true;
-      cacheName = "doctordalek1963";
-      cachixTokenFile = config.sops.secrets."cachix/tokens/doctordalek1963".path;
-      compressionLevel = 5;
-    };
-
     nix.settings = {
       substituters = [
         "https://cache.nixos.org/"
