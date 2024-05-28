f1fcc4cbed38f8a5b62e6e5c92bae520a3810c00
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-28 15:18:25 +0100

    Simplify VirtualBox-NixOS disko to no longer use LVM over LUKS

--- machines/VirtualBox-NixOS/disko.nix
+++ machines/VirtualBox-NixOS/disko.nix
@@ -16,78 +16,32 @@
                 mountpoint = "/boot";
               };
             };
-            swap = {
-              size = "4G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
-              };
-            };
-            luks = {
+            nixos = {
               size = "100%";
               content = {
-                type = "luks";
-                name = "cryptrootsda";
-                passwordFile = "/tmp/password";
-                settings.allowDiscards = true;
-
-                content = {
-                  type = "lvm_pv";
-                  vg = "pool";
+                type = "btrfs";
+                extraArgs = ["-f" "--label" "NixOS"];
+                subvolumes = let
+                  mount-options = compression-level: ["compress=zstd:${compression-level}" "noatime"];
+                in {
+                  "/rootfs" = {
+                    mountOptions = mount-options "2";
+                    mountpoint = "/";
+                  };
+                  "/home" = {
+                    mountOptions = mount-options "2";
+                    mountpoint = "/home";
+                  };
+                  "/home/.snapshots" = {
+                    mountOptions = mount-options "5";
+                    mountpoint = "/home/.snapshots";
+                  };
+                  "/nix" = {
+                    mountOptions = mount-options "2";
+                    mountpoint = "/nix";
+                  };
                 };
               };
-            };
-          };
-        };
-      };
-      sdb = {
-        device = "/dev/sdb";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions.luks = {
-            size = "100%";
-            content = {
-              type = "luks";
-              name = "cryptrootsdb";
-              passwordFile = "/tmp/password";
-              settings.allowDiscards = true;
-
-              content = {
-                type = "lvm_pv";
-                vg = "pool";
-              };
-            };
-          };
-        };
-      };
-    };
-
-    lvm_vg.pool = {
-      type = "lvm_vg";
-      lvs.root = {
-        size = "100%VG"; # The whole volume group
-        content = {
-          type = "btrfs";
-          extraArgs = ["-f" "--label" "NixOS"];
-          subvolumes = let
-            mount-options = compression-level: ["compress=zstd:${compression-level}" "noatime"];
-          in {
-            "/rootfs" = {
-              mountOptions = mount-options "2";
-              mountpoint = "/";
-            };
-            "/home" = {
-              mountOptions = mount-options "2";
-              mountpoint = "/home";
-            };
-            "/home/.snapshots" = {
-              mountOptions = mount-options "5";
-              mountpoint = "/home/.snapshots";
-            };
-            "/nix" = {
-              mountOptions = mount-options "2";
-              mountpoint = "/nix";
             };
           };
         };
