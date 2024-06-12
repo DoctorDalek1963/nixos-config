b0166c99cefd5231ad45e32696ffc04f33d6a41e
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-06-12 11:15:55 +0100

    Fix silly mistakes in Cinnamon themes file

--- home-manager/modules/desktopEnvironments/cinnamon/themes.nix
+++ home-manager/modules/desktopEnvironments/cinnamon/themes.nix
@@ -3,28 +3,28 @@
   config,
   ...
 }: let
-  cinnamonCfg = config.setup.desktopEnvironments.gnome;
+  cinnamonCfg = config.setup.desktopEnvironments.cinnamon;
 
   cursor-theme = cinnamonCfg.theme.cursor;
   gtk-theme = cinnamonCfg.theme.gtk;
 in {
   config = lib.mkIf cinnamonCfg.enable {
     dconf.settings = {
-      "/org/cinnamon/desktop/interface" = {
+      "org/cinnamon/desktop/interface" = {
         inherit gtk-theme cursor-theme;
         icon-theme = gtk-theme;
       };
 
-      "/org/gnome/desktop/interface" = {
+      "org/gnome/desktop/interface" = {
         inherit gtk-theme cursor-theme;
         icon-theme = gtk-theme;
       };
 
-      "/org/cinnamon/theme" = {
+      "org/cinnamon/theme" = {
         name = gtk-theme;
       };
 
-      "/org/x/apps/portal" = {
+      "org/x/apps/portal" = {
         color-scheme =
           if lib.hasInfix "dark" gtk-theme
           then "prefer-dark"
