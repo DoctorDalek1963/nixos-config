bfd74f34233a15ae1a07e9e1ab14023a2a473656
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-07-03 15:09:45 +0100

    Use services.hardware.openrgb and remove scheduler plugin
    Currently, this OpenRGB setup is broken. But it was also broken with my
    old config. The scheduler plugin has never worked properly and I've
    never used it, so I just got rid of it.

--- modules/openRGB/with-all-plugins.nix
+++
@@ -1,8 +0,0 @@
-{pkgs}: let
-  plugin-scheduler = pkgs.libsForQt5.callPackage ./plugins/scheduler {};
-in
-  pkgs.openrgb.withPlugins [
-    pkgs.openrgb-plugin-effects
-    pkgs.openrgb-plugin-hardwaresync
-    plugin-scheduler
-  ]
