545289bca2e31244b4b14df6a9e4f7ca2426bdb0
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-07-08 13:34:46 +0100

    Add option to put VirtualBox in a dedicated specialisation

--- modules/virtualBox/host.nix
+++ modules/virtualBox/host.nix
@@ -1,12 +1,26 @@
 {
+  pkgs,
   lib,
   config,
   ...
 }: let
+  inherit (lib) mkIf mkMerge;
   cfg = config.setup.virtualBox.host;
 in {
-  config = lib.mkIf cfg.enable {
-    virtualisation.virtualbox.host.enable = true;
-    users.groups.vboxusers.members = cfg.users;
-  };
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf cfg.asSpecialisation {
+      specialisation.virtualbox.configuration = {
+        virtualisation.virtualbox.host.enable = true;
+        users.groups.vboxusers.members = cfg.users;
+
+        # VirtualBox needs particular kernel modules that don't always compile
+        # on newer kernels, but should work on the latest LTS kernel
+        boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
+      };
+    })
+    (mkIf (!cfg.asSpecialisation) {
+      virtualisation.virtualbox.host.enable = true;
+      users.groups.vboxusers.members = cfg.users;
+    })
+  ]);
 }
--- setup.nix
+++ setup.nix
@@ -335,6 +335,7 @@ in {
           type = types.listOf types.nonEmptyStr;
           default = [];
         };
+        asSpecialisation = defaultTrue;
       };
     };
   };
