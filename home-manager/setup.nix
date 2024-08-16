7e733d891c60a67bca400d689bbaa5b3255d515b
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-08-16 15:05:21 +0100

    Disable Terminator by default

--- home-manager/setup.nix
+++ home-manager/setup.nix
@@ -66,7 +66,7 @@ in {
       };
 
       emulators = {
-        terminator = defaultTrue;
+        terminator = defaultFalse;
         wezterm = defaultTrue;
       };
       defaultEmulator = mkOption {
