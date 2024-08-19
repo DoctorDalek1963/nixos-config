45205e6b38791aa130ab89c4079715e582f33f84
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-08-19 20:19:30 +0100

    Temporarily pin nixvim-flake to older commit
    justfile syntax highlighting is currently broken, so the latest commit
    of my nixvim flake is currently impractical to use

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -16,7 +16,7 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim-flake.url = "github:DoctorDalek1963/nixvim-config";
+    nixvim-flake.url = "github:DoctorDalek1963/nixvim-config/3cdb8bd5c335548a9129901ad084900ab237c4a6";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs = {
