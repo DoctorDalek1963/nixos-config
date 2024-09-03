9db66c4f744adde4829b1f955d2e7288d97b867f
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-03 19:15:37 +0100

    Retain fewer old snapshots with snapper

--- machines/Alex-NixOS/snapper.nix
+++ machines/Alex-NixOS/snapper.nix
@@ -9,7 +9,12 @@
       ALLOW_USERS = ["dyson"];
       TIMELINE_CREATE = true;
       TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = 0;
       TIMELINE_LIMIT_DAILY = 14;
+      TIMELINE_LIMIT_WEEKLY = 0;
+      TIMELINE_LIMIT_MONTHLY = 0;
+      TIMELINE_LIMIT_QUARTERLY = 0;
+      TIMELINE_LIMIT_YEARLY = 0;
     };
   };
 }
