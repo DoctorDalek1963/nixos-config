b1aaf4ce8c6da71f068aa1ee81f39b099dcde110
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-10 20:40:45 +0100

    Provide Spotify and Last.fm authentication for Navidrome

--- modules/homeServer/mediaServer/music/navidrome.nix
+++ modules/homeServer/mediaServer/music/navidrome.nix
@@ -28,5 +28,24 @@ in {
         };
       };
     };
+
+    sops.secrets = {
+      "home-server/navidrome/spotify/id".mode = "0400";
+      "home-server/navidrome/spotify/secret".mode = "0400";
+      "home-server/navidrome/lastfm/apikey".mode = "0400";
+      "home-server/navidrome/lastfm/secret".mode = "0400";
+    };
+
+    systemd.services.navidrome.serviceConfig.ExecStart = lib.mkForce "${
+      pkgs.writeShellScript "start-navidrome.sh"
+      ''
+        ND_SPOTIFY_ID="$(cat ${config.sops.secrets."home-server/navidrome/spotify/id".path})"
+        ND_SPOTIFY_SECRET="$(cat ${config.sops.secrets."home-server/navidrome/spotify/secret".path})"
+        ND_LASTFM_APIKEY="$(cat ${config.sops.secrets."home-server/navidrome/lastfm/apikey".path})"
+        ND_LASTFM_SECRET="$(cat ${config.sops.secrets."home-server/navidrome/lastfm/secret".path})"
+
+        ${config.services.navidrome.package}/bin/navidrome --configfile ${(pkgs.formats.json {}).generate "navidrome.json" config.services.navidrome.settings}
+      ''
+    }";
   };
 }
