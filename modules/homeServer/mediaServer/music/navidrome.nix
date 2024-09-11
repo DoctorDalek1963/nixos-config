983d811292da39a38c8be1eb22b07eedfa025d37
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-11 01:16:49 +0100

    Allow Navidrome systemd service to actually find secrets

--- modules/homeServer/mediaServer/music/navidrome.nix
+++ modules/homeServer/mediaServer/music/navidrome.nix
@@ -29,23 +29,38 @@ in {
       };
     };
 
-    sops.secrets = {
-      "home-server/navidrome/spotify/id".mode = "0400";
-      "home-server/navidrome/spotify/secret".mode = "0400";
-      "home-server/navidrome/lastfm/apikey".mode = "0400";
-      "home-server/navidrome/lastfm/secret".mode = "0400";
+    sops.secrets = let
+      conf = {
+        mode = "0440";
+        owner = config.systemd.services.navidrome.serviceConfig.User;
+        group = config.systemd.services.navidrome.serviceConfig.Group;
+      };
+    in {
+      "home-server/navidrome/spotify/id" = conf;
+      "home-server/navidrome/spotify/secret" = conf;
+      "home-server/navidrome/lastfm/apikey" = conf;
+      "home-server/navidrome/lastfm/secret" = conf;
     };
 
-    systemd.services.navidrome.serviceConfig.ExecStart = lib.mkForce "${
-      pkgs.writeShellScript "start-navidrome.sh"
-      ''
-        ND_SPOTIFY_ID="$(cat ${config.sops.secrets."home-server/navidrome/spotify/id".path})"
-        ND_SPOTIFY_SECRET="$(cat ${config.sops.secrets."home-server/navidrome/spotify/secret".path})"
-        ND_LASTFM_APIKEY="$(cat ${config.sops.secrets."home-server/navidrome/lastfm/apikey".path})"
-        ND_LASTFM_SECRET="$(cat ${config.sops.secrets."home-server/navidrome/lastfm/secret".path})"
+    systemd.services.navidrome.serviceConfig = {
+      BindReadOnlyPaths = [
+        config.sops.secrets."home-server/navidrome/spotify/id".path
+        config.sops.secrets."home-server/navidrome/spotify/secret".path
+        config.sops.secrets."home-server/navidrome/lastfm/apikey".path
+        config.sops.secrets."home-server/navidrome/lastfm/secret".path
+      ];
 
-        ${config.services.navidrome.package}/bin/navidrome --configfile ${(pkgs.formats.json {}).generate "navidrome.json" config.services.navidrome.settings}
-      ''
-    }";
+      ExecStart = lib.mkForce "${
+        pkgs.writeShellScript "start-navidrome.sh"
+        ''
+          export ND_SPOTIFY_ID="$(cat ${config.sops.secrets."home-server/navidrome/spotify/id".path})"
+          export ND_SPOTIFY_SECRET="$(cat ${config.sops.secrets."home-server/navidrome/spotify/secret".path})"
+          export ND_LASTFM_APIKEY="$(cat ${config.sops.secrets."home-server/navidrome/lastfm/apikey".path})"
+          export ND_LASTFM_SECRET="$(cat ${config.sops.secrets."home-server/navidrome/lastfm/secret".path})"
+
+          ${config.services.navidrome.package}/bin/navidrome --configfile ${(pkgs.formats.json {}).generate "navidrome.json" config.services.navidrome.settings}
+        ''
+      }";
+    };
   };
 }
