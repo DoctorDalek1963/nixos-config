ee0f69107bf91077f75c02f92d290c874f6aa58c
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-11 23:07:18 +0100

    Separate Octave config and add symbolic package

--- home-manager/modules/maths/default.nix
+++ home-manager/modules/maths/default.nix
@@ -8,19 +8,16 @@
 }: let
   cfg = config.setup.maths;
 in {
-  imports = [./sage.nix];
+  imports = [./octave.nix ./sage.nix];
 
   config = lib.mkIf cfg.enable {
     home.packages =
       lib.optional cfg.lintrans inputs.lintrans.packages."${system}".default
-      ++ lib.optional cfg.octave pkgs.octaveFull
       ++ lib.optional cfg.tikzit pkgs.tikzit
       ++ lib.optional cfg.weylus pkgs.weylus
       ++ lib.optional cfg.zotero pkgs.zotero;
 
     setup.impermanence = {
-      keepFiles = lib.optional cfg.octave ".local/share/octave/history";
-
       keepDirs =
         [".cache/mathlib"] # Mathlib for Lean 4, which I only use in devShells
         ++ lib.optional cfg.lintrans ".lintrans"
