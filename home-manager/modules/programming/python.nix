f555829c505d316ff2cac012366e99e1b857c919
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-28 22:36:22 +0100

    Inline IPython config

--- home-manager/modules/programming/python.nix
+++ home-manager/modules/programming/python.nix
@@ -35,7 +35,61 @@ in {
   config = lib.mkIf config.setup.programming.python {
     home = {
       packages = [python];
-      file.".ipython/profile_default/ipython_config.py".source = ../../files/ipython_config.py;
+      file.".ipython/profile_default/ipython_config.py".text =
+        # python
+        ''
+          c.InteractiveShell.confirm_exit = False
+          c.InteractiveShell.autocall = 1
+          c.InteractiveShell.colors = "Linux"
+
+          c.InteractiveShellApp.exec_lines = [
+              "%load_ext autoreload",
+              "%autoreload 2",
+              """
+          import itertools
+          import operator
+          import os
+          import re
+          import sys
+          import time
+
+          from datetime import datetime as dt
+          from datetime import timedelta as td
+
+          import math as ma
+
+          import matplotlib as mpl
+          import matplotlib.pyplot as plt
+
+          import numpy as np
+          from numpy import linalg as la
+
+          import pandas as pd
+
+          import scipy
+
+          import sympy as sp
+
+          import bitstring
+
+          from probcalc import *
+
+          a, b, c, d, i, j, l, r, t, u, v, w, x, y, z = sp.symbols("a b c d i j l r t u v w x y z")
+          k, m, n, p, q = sp.symbols("k m n p q", integer=True)
+          f, g, h = sp.symbols("f g h", cls=sp.Function)
+          sp.init_printing()
+
+          def matrix_minors(matrix: sp.Matrix) -> sp.Matrix:
+              return sp.Matrix([
+                  [matrix.minor(row, col) for col in range(matrix.T.rows)]
+                  for row in range(matrix.rows)
+              ])
+
+          def get_bits(*args, **kwargs) -> str:
+              return bitstring.BitArray(*args, **kwargs).bin
+          """,
+          ]
+        '';
     };
 
     setup = {
