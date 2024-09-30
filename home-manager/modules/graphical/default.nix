e82ab350296638e8e864553f1c3bf7454f3a0d0b
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-30 21:36:22 +0100

    Add AutogainWithNoiseReduction EasyEffects preset

--- home-manager/modules/graphical/default.nix
+++ home-manager/modules/graphical/default.nix
@@ -9,7 +9,17 @@
       preset = "BasicAutogain";
     };
 
-    xdg.configFile = {
+    xdg.configFile = let
+      basic-autogain = {
+        bypass = false;
+        input-gain = 0.0;
+        output-gain = 0.0;
+        maximum-history = 10;
+        reference = "Geometric Mean (MSI)";
+        silence-threshold = -70.0;
+        target = -18.0;
+      };
+    in {
       "easyeffects/output/Nothing.json".text = lib.generators.toJSON {} {
         output = {
           blocklist = [];
@@ -18,17 +28,26 @@
       };
       "easyeffects/output/BasicAutogain.json".text = lib.generators.toJSON {} {
         output = {
-          "autogain#0" = {
-            bypass = false;
-            input-gain = 0.0;
-            output-gain = 0.0;
-            maximum-history = 10;
-            reference = "Geometric Mean (MSI)";
-            silence-threshold = -70.0;
-            target = -18.0;
-          };
+          "autogain#0" = basic-autogain;
           blocklist = [];
           plugins_order = ["autogain#0"];
+        };
+      };
+      "easyeffects/output/AutogainWithNoiseReduction.json".text = lib.generators.toJSON {} {
+        output = {
+          "autogain#0" = basic-autogain;
+          "rnnoise#0" = {
+            bypass = false;
+            enable-vad = false;
+            input-gain = 0.0;
+            model-path = "";
+            output-gain = 0.0;
+            release = 20.0;
+            vad-thres = 50.0;
+            wet = 0.0;
+          };
+          blocklist = [];
+          plugins_order = ["autogain#0" "rnnoise#0"];
         };
       };
     };
