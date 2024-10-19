59b38bd64d7409d5633bb86c5b76e390bfee99fa
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-10-19 15:57:47 +0100

    Add keys for cache.nixos.org and hydra.nixos.org and add Lix cache to ISO

--- modules/secrets/cachix.nix
+++ modules/secrets/cachix.nix
@@ -29,6 +29,8 @@ in {
         "https://doctordalek1963.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
         "doctordalek1963.cachix.org-1:ide2OUuSBdJY4mSTyIanZaafJhcHNA5fFh6P633b8EI="
