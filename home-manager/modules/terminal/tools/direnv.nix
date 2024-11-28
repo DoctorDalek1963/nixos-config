e6c79e54e6bf3d8917fa7f3a95321c454170aa0d
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-11-28 16:59:25 +0000

    Tell direnv to hide diff of env vars

--- home-manager/modules/terminal/tools/direnv.nix
+++ home-manager/modules/terminal/tools/direnv.nix
@@ -7,7 +7,10 @@
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+
       enableBashIntegration = config.setup.terminal.shells.bash;
+
+      config.global.hide_env_diff = true;
     };
 
     setup = {
