1d670ad8c8a03529ee0e4ed3ed65125e9964b3f9
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-12-15 22:17:13 +0000

    Write basic (broken) config for Steam gamescope session

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -5,14 +5,50 @@
   ...
 }: let
   cfg = config.setup.gaming;
+  # steam-gamescope = pkgs.writeShellScriptBin "steam-gamescope" ''
+  #   gamescope \
+  #     --backend sdl \
+  #     -w 1920 -h 1080 \
+  #     --fullscreen \
+  #     --prefer-vk-device 1002:747e \
+  #     --steam \
+  #     -- \
+  #     steam -gamepadui -steamos3
+  # '';
 in {
   config = lib.mkIf (cfg.enable && cfg.steam.enable) {
-    programs.steam = {
-      enable = true;
-      extraCompatPackages =
-        if cfg.steam.enableProtonGE
-        then [pkgs.proton-ge-bin]
-        else [];
+    # environment.systemPackages = [steam-gamescope];
+
+    programs = {
+      gamescope = {
+        enable = true;
+        capSysNice = true;
+        args = [
+          "--rt"
+          "--prefer-vk-device 1002:747e"
+        ];
+      };
+
+      steam = {
+        enable = true;
+        gamescopeSession = {
+          enable = true;
+          # args = ["-w" "1920" "-h" "1080" "--fullscreen"];
+        };
+
+        extraCompatPackages =
+          if cfg.steam.enableProtonGE
+          then [pkgs.proton-ge-bin]
+          else [];
+
+        package = pkgs.steam.override {
+          extraPkgs = p:
+            with p; [
+              libkrb5
+              keyutils
+            ];
+        };
+      };
     };
   };
 }
