f3c583562beecfd93de4f9b69484d25297612361
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-12-20 17:53:24 +0000

    Make Hyprland GTK config adapt to hyprland.theme

--- home-manager/modules/desktopEnvironments/hyprland/gtk.nix
+++ home-manager/modules/desktopEnvironments/hyprland/gtk.nix
@@ -4,42 +4,56 @@
   config,
   osConfig,
   ...
-}: {
+}: let
+  theme-config =
+    {
+      "catppuccin-macchiato-mauve" = {
+        home.pointerCursor = {
+          package = pkgs.catppuccin-cursors.macchiatoLight;
+          name = "catppuccin-macchiato-light-cursors";
+        };
+
+        gtk = {
+          theme = {
+            package = pkgs.catppuccin-gtk.override {
+              accents = ["mauve"];
+              size =
+                if config.setup.isLaptop
+                then "compact"
+                else "standard";
+              tweaks = ["rimless"];
+              variant = "macchiato"; # Dark mode
+            };
+            name =
+              if config.setup.isLaptop
+              then "catppuccin-macchiato-mauve-compact+rimless"
+              else "catppuccin-macchiato-mauve-standard+rimless";
+          };
+
+          iconTheme = {
+            package = pkgs.catppuccin-papirus-folders.override {
+              accent = "mauve";
+              flavor = "macchiato"; # Dark mode
+            };
+            name = "Papirus-Dark";
+          };
+        };
+      };
+    }
+    .${config.setup.desktopEnvironments.hyprland.theme};
+in {
   config = lib.mkIf osConfig.setup.desktopEnvironments.hyprland.enable {
-    # TODO: Have a setup.desktopEnvironments.hyprland.theme setting?
     home.pointerCursor = {
       gtk.enable = true;
-      package = pkgs.catppuccin-cursors.macchiatoLight;
-      name = "catppuccin-macchiato-light-cursors";
       size = 24;
+
+      inherit (theme-config.home.pointerCursor) package name;
     };
 
     gtk = {
       enable = true;
 
-      theme = {
-        package = pkgs.catppuccin-gtk.override {
-          accents = ["mauve"];
-          size =
-            if config.setup.isLaptop
-            then "compact"
-            else "standard";
-          tweaks = ["rimless"];
-          variant = "macchiato"; # Dark mode
-        };
-        name =
-          if config.setup.isLaptop
-          then "catppuccin-macchiato-mauve-compact+rimless"
-          else "catppuccin-macchiato-mauve-standard+rimless";
-      };
-
-      iconTheme = {
-        package = pkgs.catppuccin-papirus-folders.override {
-          accent = "mauve";
-          flavor = "macchiato"; # Dark mode
-        };
-        name = "Papirus-Dark";
-      };
+      inherit (theme-config.gtk) theme iconTheme;
     };
   };
 }
