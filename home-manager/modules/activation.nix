fec5d8d739ca6f0ef965cc6581300e1f69716c8f
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-12-21 19:45:56 +0000

    Add home-manager activation script to rm ~/.ssh/known-hosts.hm-backup
    I'm sick of manually removing it all the time

--- home-manager/modules/activation.nix
+++ home-manager/modules/activation.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  osConfig,
   ...
 }: let
   fillIf = condition: content:
@@ -17,6 +18,16 @@
 
   cfg = config.setup;
 
+  # This actually removes the file after we back it up. So
+  # known_hosts.hm-backup gets created and deleted every time we switch a new
+  # config. But this is way better than having to manually remove it all the time
+  rmSshKnownHosts = fillIf osConfig.setup.ssh.enable {
+    rmSshKnownHosts = let
+      filename = ".ssh/known_hosts.${osConfig.home-manager.backupFileExtension}";
+    in
+      mkScript "$DRY_RUN_CMD rm -f ${config.home.homeDirectory}/${filename}";
+  };
+
   restartRcloneMounts =
     fillIf cfg.rclone.enable
     (builtins.listToAttrs (builtins.map ({remote, ...}: {
@@ -28,5 +39,9 @@
   restartSopsNix = fillIf cfg.secrets.enable {restartSopsNix = mkSystemdRestart "sops-nix";};
   restartXremap = fillIf cfg.misc.programs.xremap {restartXremap = mkSystemdRestart "xremap";};
 in {
-  home.activation = restartRcloneMounts // restartSopsNix // restartXremap;
+  home.activation =
+    rmSshKnownHosts
+    // restartRcloneMounts
+    // restartSopsNix
+    // restartXremap;
 }
