82987cc3414da2d1697706602dc0bdfb3b74624f
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-12-22 01:07:39 +0000

    Fix and simplify gamescope-session config

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -5,36 +5,17 @@
   ...
 }: let
   cfg = config.setup.gaming;
-  # steam-gamescope = pkgs.writeShellScriptBin "steam-gamescope" ''
-  #   gamescope \
-  #     --backend sdl \
-  #     -w 1920 -h 1080 \
-  #     --fullscreen \
-  #     --prefer-vk-device 1002:747e \
-  #     --steam \
-  #     -- \
-  #     steam -gamepadui -steamos3
-  # '';
 in {
   config = lib.mkIf (cfg.enable && cfg.steam.enable) {
-    # environment.systemPackages = [steam-gamescope];
-
     programs = {
       gamescope = {
         enable = true;
         capSysNice = true;
-        args = [
-          "--rt"
-          "--prefer-vk-device 1002:747e"
-        ];
       };
 
       steam = {
         enable = true;
-        gamescopeSession = {
-          enable = true;
-          # args = ["-w" "1920" "-h" "1080" "--fullscreen"];
-        };
+        gamescopeSession.enable = true;
 
         extraCompatPackages =
           if cfg.steam.enableProtonGE
