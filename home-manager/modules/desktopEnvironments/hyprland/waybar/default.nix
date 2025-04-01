dc51350468d3d28aa3034539ee6c7de72d594c3d
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-04-01 21:13:45 +0100

    Reset network connection when right clicking waybar network module

--- home-manager/modules/desktopEnvironments/hyprland/waybar/default.nix
+++ home-manager/modules/desktopEnvironments/hyprland/waybar/default.nix
@@ -152,6 +152,13 @@
             tooltip-format = "{bandwidthUpBytes} 󰜷 {bandwidthDownBytes} 󰜮 ({ipaddr}/{cidr} on {ifname})";
 
             on-click = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
+            on-click-right = let
+              nmcli = "${pkgs.networkmanager}/bin/nmcli";
+              tailscale = "${osConfig.services.tailscale.package}/bin/tailscale";
+            in
+              if osConfig.services.tailscale.enable
+              then "${nmcli} networking off; ${tailscale} down; sleep 5; ${nmcli} networking on; ${tailscale} up"
+              else "${nmcli} networking off; sleep 5; ${nmcli} networking on";
           };
 
           bluetooth = lib.mkIf osConfig.setup.hasBluetooth {
