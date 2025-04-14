9cc11279a9370334d5bdaccc56b229d805bd8b96
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-04-15 00:08:46 +0100

    Add todo comment to update servarr config in 25.05

--- modules/homeServer/mediaServer/default.nix
+++ modules/homeServer/mediaServer/default.nix
@@ -9,6 +9,8 @@ in {
   imports = [./torrenting ./servarrConfig ./books ./music ./video ./prowlarr.nix];
 
   config = lib.mkIf (cfg.enable && cfgMs.enable) {
+    # TODO (25.05): Change all servarr stuff to use the new declarative config style
+
     users.groups.media = {};
 
     systemd.tmpfiles.rules = ["d ${cfgMs.mediaRoot} 0775 root media - -"];
