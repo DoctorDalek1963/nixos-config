d58e661211ee24de25bd107a28143b1e22bdcc5e
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-04-16 00:08:09 +0100

    Fix weird issue with rsync.net SSH private key

--- modules/backup/borg.nix
+++ modules/backup/borg.nix
@@ -25,6 +25,8 @@ in {
     services.borgbackup.jobs.automatic-rsync-net =
       lib.mkIf
       (cfg.enable && builtins.length cfg.paths > 0) {
+        group = "backup";
+
         inherit (cfg) paths;
         exclude = ["/nix"] ++ cfg.exclude;
 
@@ -41,9 +43,30 @@ in {
           passCommand = "cat ${config.sops.secrets."borgbackup/repos/rsync.net/nixos-backups/passphrase".path}";
         };
 
-        environment = {
-          BORG_RSH = "ssh -i ${config.sops.secrets."ssh/rsync.net/keys/rsync_net".path}";
-        };
+        environment.BORG_RSH = "ssh -i ${config.sops.secrets."ssh/rsync.net/keys/rsync_net".path}";
+
+        # This is bizarre. Basically, the SSH private key for rsync.net has
+        # mode 0440 so that any user in the backup group (like my normal user)
+        # can access it. For those users, this is fine. But for some reason,
+        # when running the systemd service, we get an SSH error that the
+        # permissions are too open.
+
+        # I believe this is because the service is run as root and the private
+        # key is owned by root, but even if I make a new user specifically to
+        # own the key, I still get the permission error. Perhaps it only works
+        # for normal users because I symlink the keys to ~/.ssh.
+
+        # Either way, copying the private key and changing the mode works as a
+        # little hack just for the service.
+        preHook = ''
+          sshKeyDir="$(mktemp -d)"
+          trap "rm -rf $sshKeyDir" EXIT
+
+          cp ${config.sops.secrets."ssh/rsync.net/keys/rsync_net".path} $sshKeyDir/rsync_net
+          chmod 0400 $sshKeyDir/rsync_net
+
+          export BORG_RSH="ssh -i $sshKeyDir/rsync_net"
+        '';
 
         compression = "auto,lzma";
         inherit (cfg) startAt;
