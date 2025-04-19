675ea3f521f39b444d4bc76e2700fad8f694c5c7
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-04-19 01:17:19 +0100

    Fix weird permissions bodge with rsync.net key for borg backups

--- modules/backup/borg.nix
+++ modules/backup/borg.nix
@@ -67,29 +67,6 @@ in {
         };
 
         environment.BORG_RSH = "ssh -i ${config.sops.secrets."ssh/rsync.net/keys/rsync_net".path}";
-
-        # This is bizarre. Basically, the SSH private key for rsync.net has
-        # mode 0440 so that any user in the backup group (like my normal user)
-        # can access it. For those users, this is fine. But for some reason,
-        # when running the systemd service, we get an SSH error that the
-        # permissions are too open.
-
-        # I believe this is because the service is run as root and the private
-        # key is owned by root, but even if I make a new user specifically to
-        # own the key, I still get the permission error. Perhaps it only works
-        # for normal users because I symlink the keys to ~/.ssh.
-
-        # Either way, copying the private key and changing the mode works as a
-        # little hack just for the service.
-        preHook = ''
-          sshKeyDir="$(mktemp -d)"
-          trap "rm -rf $sshKeyDir" EXIT
-
-          cp ${config.sops.secrets."ssh/rsync.net/keys/rsync_net".path} $sshKeyDir/rsync_net
-          chmod 0400 $sshKeyDir/rsync_net
-
-          export BORG_RSH="ssh -i $sshKeyDir/rsync_net"
-        '';
       };
     })
     (lib.mkIf (cfg.enable && cfg.ntfy.url != null) {
--- modules/backup/rsyncNet.nix
+++ modules/backup/rsyncNet.nix
@@ -7,16 +7,23 @@
   cfg = config.setup.backup;
 in {
   config = lib.mkIf cfg.enable {
+    # These secrets are owned by nobody and group readable so that any user in
+    # the backup group can use them, for example in the generated wrapper
+    # script that uses BORG_RSH. The reason they have to be owned by nobody is
+    # because openssh doesn't lack lax permission, like group readability. But
+    # it only does the permissions check if the current user owns the file.
+    # Otherwise, it just tries to read it. Since nobody owns these files, any
+    # user in backup can read them!
     sops.secrets = {
       "ssh/rsync.net/keys/rsync_net" = {
-        owner = "root";
+        owner = "nobody";
         group = "backup";
-        mode = "0440";
+        mode = "0040";
       };
       "ssh/rsync.net/keys/rsync_net.pub" = {
-        owner = "root";
+        owner = "nobody";
         group = "backup";
-        mode = "0444";
+        mode = "0040";
       };
     };
 
