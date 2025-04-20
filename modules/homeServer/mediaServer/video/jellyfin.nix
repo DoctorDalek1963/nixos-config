ba4d1a1c819d6d28d6ae7a1a4c4846e19ec74f63
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-04-20 18:53:27 +0100

    Persist Jellyfin cache directory

--- modules/homeServer/mediaServer/video/jellyfin.nix
+++ modules/homeServer/mediaServer/video/jellyfin.nix
@@ -8,7 +8,10 @@
 in {
   config = lib.mkIf (cfg.enable && cfgMs.enable && (cfgMs.movies || cfgMs.telly)) {
     # TODO: Next time I enable this, add setup.backup.paths for databases etc.
-    setup.impermanence.keepDirs = [config.services.jellyfin.dataDir];
+    setup.impermanence.keepDirs = [
+      config.services.jellyfin.dataDir
+      config.services.jellyfin.cacheDir
+    ];
 
     # NOTE: On a fresh install, you first need to connect to
     # http://{ip_address}:8096 and add libraries, then enable HTTPS and set the
