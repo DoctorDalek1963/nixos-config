1db6dbcc718f23ffb6514ed6f1c7f423f71a6d85
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-03 02:54:19 +0100

    Remove silly container

--- modules/homeServer/mediaServer/video/bazarr/whisper.nix
+++ modules/homeServer/mediaServer/video/bazarr/whisper.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -21,31 +20,6 @@ in {
 
     systemd.tmpfiles.settings.whisperAsr."${cacheDir}".d = cacheDirPerms;
 
-    # I really don't want to use an OCI container here but packaging the
-    # underlying Python app was a massive headache, and the docker image is
-    # provided and maintained
-    virtualisation.oci-containers.containers.whisper-asr-webservice = {
-      image = "onerahmet/openai-whisper-asr-webservice:v1.8.2-gpu";
-      # pull = "never"; # TODO (25.05): Uncomment this
-      imageFile = pkgs.dockerTools.pullImage {
-        imageName = "onerahmet/openai-whisper-asr-webservice";
-        finalImageTag = "v1.8.2-gpu";
-        imageDigest = "sha256:75f21f1fffdb69f40f9f1dccfeb9f581c14242b4b43c5437e0910c94f2bb51d4";
-        sha256 = "1l386gj5jls0hwfs40hpyhzd0hb3qv690sl08c9hbbv5l341mb6j";
-      };
-
-      # podman.user = "bazarr"; # TODO (25.05): Uncomment this
-
-      # We specify 127.0.0.1 here because otherwise, NixOS would open this port in the firewall
-      ports = ["127.0.0.1:${toString cfg.ports.mediaServer.whisper-asr}:9000"];
-      volumes = ["${cacheDir}:/root/.cache"];
-
-      environment = {
-        ASR_MODEL = "base";
-        ASR_ENGINE = "openai_whisper";
-      };
-
-      extraOptions = ["--gpus=all"];
-    };
+    # TODO: Replace container with native nix package, probably external flake with poetry2nix?
   };
 }
