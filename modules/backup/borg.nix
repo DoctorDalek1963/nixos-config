2a462a623d0cb36aa5cea298f7dc8d36adb7ff39
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-07 00:24:33 +0100

    Uncomment extra borg args

--- modules/backup/borg.nix
+++ modules/backup/borg.nix
@@ -41,9 +41,8 @@ in {
         inhibitsSleep = true;
         archiveBaseName = "${config.setup.hostname}-auto";
 
-        # TODO (25.05): Uncomment these when the borg service expands extraArgs properly
-        # extraArgs = ["--remote-path=borg14"];
-        # extraCreateArgs = ["--stats" "--checkpoint-interval 600"]
+        extraArgs = ["--remote-path=borg14"];
+        extraCreateArgs = ["--stats" "--checkpoint-interval=600"];
 
         # An empty list here makes the service not start automatically, but
         # only be triggered manually. When we have no paths, we obviously don't
