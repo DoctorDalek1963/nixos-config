acdf54d006816bac72a29e1edfc3c6cd0a764a3f
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-08 22:45:20 +0100

    Move directoryMap setup logic to separate file

--- modules/homeServer/mediaServer/default.nix
+++ modules/homeServer/mediaServer/default.nix
@@ -1,27 +1,10 @@
 {
-  pkgs,
   lib,
   config,
   ...
 }: let
   cfg = config.setup.homeServer;
   cfgMs = cfg.mediaServer;
-
-  directoryMapTmpfiles =
-    lib.mergeAttrsList
-    (lib.attrValues
-      (lib.mapAttrs
-        (user: paths:
-          lib.listToAttrs (map (path:
-            lib.nameValuePair "${path}" {
-              d = {
-                inherit user;
-                group = "media";
-                mode = "775";
-              };
-            })
-          paths))
-        cfgMs.directoryMap));
 in {
   imports = [
     ./torrenting
@@ -31,65 +14,13 @@ in {
     ./video
     ./fileflows
 
+    ./directoryMap.nix
     ./prowlarr.nix
   ];
 
   config = lib.mkIf (cfg.enable && cfgMs.enable) {
     users.groups.media = {
       members = lib.optional (cfg.manager != null) cfg.manager;
-    };
-
-    systemd = {
-      # We can't declare mediaRoot in directoryMap because the permissions
-      # service does chown recursively and there's no point changing all the
-      # ownership to root and back every time
-      tmpfiles.settings.mediaServer =
-        {
-          "${cfgMs.mediaRoot}".d = {
-            user = "root";
-            group = "media";
-            mode = "775";
-          };
-        }
-        // directoryMapTmpfiles;
-
-      services.set-permissions-for-media-server-directory-map = {
-        description = "Set ownership and permissions for the mediaServer directoryMap";
-        script = let
-          commands = lib.flatten (lib.mapAttrsToList (user: paths:
-            map (path: ''
-              chown -R ${user}:media "${path}"
-              chmod -R u=rwX,g=rwX,o=rX "${path}"
-            '')
-            paths)
-          cfgMs.directoryMap);
-
-          shellApp = pkgs.writeShellApplication {
-            name = "set-permissions-for-media-server-directory-map";
-            runtimeInputs = [pkgs.coreutils];
-            text = ''
-              chmod -R u=rwX,g=rwX,o=rX "${cfgMs.mediaRoot}"
-
-              ${lib.concatStringsSep "\n" commands}
-            '';
-          };
-        in "${shellApp}/bin/set-permissions-for-media-server-directory-map";
-
-        after = ["systemd-tmpfiles-setup.service"];
-        wants = ["systemd-tmpfiles-setup.service"];
-        wantedBy = ["multi-user.target"];
-
-        serviceConfig.Type = "simple";
-      };
-
-      timers.set-permissions-for-media-server-directory-map = {
-        timerConfig = {
-          OnCalendar = "*:00"; # Every hour
-          Unit = "set-permissions-for-media-server-directory-map.service";
-        };
-
-        wantedBy = ["timers.target"];
-      };
     };
 
     setup.backup.paths = [cfgMs.mediaRoot];
