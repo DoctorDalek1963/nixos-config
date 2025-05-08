e8b1ba6ea1571eaf8dc2d87f7d3284be24c6afff
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-08 21:47:37 +0100

    Add Jellyfin dataDir to backups

--- modules/homeServer/mediaServer/video/jellyfin.nix
+++ modules/homeServer/mediaServer/video/jellyfin.nix
@@ -7,11 +7,17 @@
   cfgMs = cfg.mediaServer;
 in {
   config = lib.mkIf (cfg.enable && cfgMs.enable && (cfgMs.movies || cfgMs.telly)) {
-    # TODO: Next time I enable this, add setup.backup.paths for databases etc.
-    setup.impermanence.keepDirs = [
-      config.services.jellyfin.dataDir
-      config.services.jellyfin.cacheDir
-    ];
+    setup = {
+      impermanence.keepDirs = [
+        config.services.jellyfin.dataDir
+        config.services.jellyfin.cacheDir
+      ];
+
+      backup = {
+        paths = [config.services.jellyfin.dataDir];
+        exclude = ["${config.services.jellyfin.dataDir}/log"];
+      };
+    };
 
     # NOTE: On a fresh install, you first need to connect to
     # http://{ip_address}:8096 and add libraries, then enable HTTPS and set the
