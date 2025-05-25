bca799e56a72a1bfb5e141183e61d29edb09eca6
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-25 18:40:25 +0100

    Use blehook PRECMD for direnv hook
    This fixes an ordering bug with Starship prompts

--- home-manager/modules/terminal/tools/direnv.nix
+++ home-manager/modules/terminal/tools/direnv.nix
@@ -2,21 +2,54 @@
   lib,
   config,
   ...
-}: {
-  config = lib.mkIf config.setup.terminal.tools.direnv {
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
+}: let
+  cfg = config.setup.terminal;
+in {
+  config = lib.mkMerge [
+    (lib.mkIf cfg.tools.direnv {
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
 
-      enableBashIntegration = config.setup.terminal.shells.bash;
+        config.global.hide_env_diff = true;
+      };
 
-      config.global.hide_env_diff = true;
-    };
+      setup = {
+        impermanence.keepDirs = [".local/share/direnv"];
+        terminal.shellAliases.ndr = "nix-direnv-reload";
+      };
+    })
+    (lib.mkIf (cfg.tools.direnv && cfg.shells.bash) {
+      programs = {
+        direnv.enableBashIntegration = false;
 
-    setup = {
-      impermanence.keepDirs = [".local/share/direnv"];
+        # Copied mostly from the output of "direnv hook bash" but adapted to use
+        # blesh. This fixes a bug with Starship where the Starship prompt would
+        # run in blesh's PRECMD before _direnv_hook would run in PROMPT_COMMAND
+        # TODO (direnv): Keep up to date. This version copied from 2.36.0
+        bash.initExtra =
+          # bash
+          ''
+            _direnv_hook() {
+              local previous_exit_status=$?;
+              trap -- "" SIGINT;
+              eval "$("${lib.getExe config.programs.direnv.package}" export bash)";
+              trap - SIGINT;
+              return $previous_exit_status;
+            };
 
-      terminal.shellAliases.ndr = "nix-direnv-reload";
-    };
-  };
+            if [[ ''${BLE_VERSION-} && _ble_version -ge 400 ]]; then
+              blehook PRECMD!="_direnv_hook"
+            elif [[ ";''${PROMPT_COMMAND[*]:-};" != *";_direnv_hook;"* ]]; then
+              if [[ "$(declare -p PROMPT_COMMAND 2>&1)" == "declare -a"* ]]; then
+                PROMPT_COMMAND=(_direnv_hook "''${PROMPT_COMMAND[@]}")
+              else
+                PROMPT_COMMAND="_direnv_hook''${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+              fi
+            fi
+
+          '';
+      };
+    })
+  ];
 }
