13e42c0ca82bdec9de5dbe124418cfb31bc6edec
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-27 12:40:29 +0100

    Integrate direnv blesh fix with patch on package rather than .bashrc snippet

--- home-manager/modules/terminal/tools/direnv.nix
+++ home-manager/modules/terminal/tools/direnv.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -21,39 +22,21 @@ in {
     })
     (lib.mkIf (cfg.tools.direnv && cfg.shells.bash) {
       programs = {
-        direnv.enableBashIntegration = false;
+        # TODO (direnv 2.37): Once https://github.com/direnv/direnv/pull/1439
+        # is merged and in a release, we can remove this patch
+        direnv = {
+          package = pkgs.direnv.overrideAttrs (oldAttrs: {
+            patches = (oldAttrs.patches or []) ++ [./direnv-bash-hook.patch];
+          });
 
-        # Copied mostly from the output of "direnv hook bash" but adapted to use
-        # blesh. This fixes a bug with Starship where the Starship prompt would
-        # run in blesh's PRECMD before _direnv_hook would run in PROMPT_COMMAND
+          enableBashIntegration = false;
+        };
 
-        # TODO (direnv): Keep up to date. This version copied from 2.36.0
-
-        # Once https://github.com/direnv/direnv/pull/1439 is merged and
-        # included in a release, we can go back to home-managers's Bash
-        # integration option
-        bash.initExtra =
-          # bash
-          ''
-            _direnv_hook() {
-              local previous_exit_status=$?;
-              trap -- "" SIGINT;
-              eval "$("${lib.getExe config.programs.direnv.package}" export bash)";
-              trap - SIGINT;
-              return $previous_exit_status;
-            };
-
-            if [[ ''${BLE_VERSION-} && _ble_version -ge 400 ]]; then
-              blehook PRECMD!="_direnv_hook"
-            elif [[ ";''${PROMPT_COMMAND[*]:-};" != *";_direnv_hook;"* ]]; then
-              if [[ "$(declare -p PROMPT_COMMAND 2>&1)" == "declare -a"* ]]; then
-                PROMPT_COMMAND=(_direnv_hook "''${PROMPT_COMMAND[@]}")
-              else
-                PROMPT_COMMAND="_direnv_hook''${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
-              fi
-            fi
-
-          '';
+        # We need direnv to hook into ble.sh before Starship, so we don't want
+        # the `mkAfter` from home-manager's implementation
+        bash.initExtra = ''
+          eval "$(${lib.getExe config.programs.direnv.package} hook bash)"
+        '';
       };
     })
   ];
