8b0f7c842596c5e17cb581578ac62647528851d6
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-06-04 17:52:44 +0100

    Shorten PAM fail delay in greetd

--- modules/pam/default.nix
+++ modules/pam/default.nix
@@ -5,6 +5,7 @@
 }: let
   inherit (lib) mkIf;
   cfg = config.setup;
+
   shortenFailDelay = {
     nodelay = true;
     failDelay = {
@@ -16,6 +17,7 @@ in {
   # Shorten delay when getting password wrong
   security.pam.services = mkIf cfg.pamShortenFailDelay.enable {
     gdm-password = mkIf cfg.displayManagers.gdm.enable shortenFailDelay;
+    greetd = mkIf cfg.displayManagers.tuigreet.enable shortenFailDelay;
     hyprlock = mkIf cfg.desktopEnvironments.hyprland.enable shortenFailDelay;
     login = shortenFailDelay;
     sudo = mkIf config.security.sudo.enable shortenFailDelay;
