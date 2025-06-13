8c8867f838d4cf2a39599aa77bc749c909620081
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-06-13 12:11:18 +0100

    Set Zathura as default PDF viewer

--- home-manager/modules/misc/programs/zathura.nix
+++ home-manager/modules/misc/programs/zathura.nix
@@ -69,5 +69,12 @@ in {
         }
         // theme;
     };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = ["org.pwmt.zathura.desktop"];
+      };
+    };
   };
 }
