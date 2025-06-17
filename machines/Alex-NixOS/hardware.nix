8d56af1b1ff4c37bdef4f38ddc5b73df057d8433
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-06-17 03:26:48 +0100

    Explicitly enable hardware.graphics on Alex

--- machines/Alex-NixOS/hardware.nix
+++ machines/Alex-NixOS/hardware.nix
@@ -33,6 +33,11 @@
 
   nix.settings.max-jobs = 12;
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
