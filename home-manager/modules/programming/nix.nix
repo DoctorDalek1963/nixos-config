0f3bd0b4ee0565fd47c60e6e91f9ff0ea5d36c3a
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-06-21 16:22:22 +0100

    Use custom nh with patch to show output in `nh clean` store gc

--- home-manager/modules/programming/nix.nix
+++ home-manager/modules/programming/nix.nix
@@ -3,18 +3,22 @@
   lib,
   config,
   ...
-}: {
+}: let
+  custom-nh = pkgs.nh.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or []) ++ [./nh_clean_show_output.patch];
+  });
+in {
   config = lib.mkIf config.setup.programming.nix {
     home.packages = with pkgs; [
-      nh
+      custom-nh
       nix-output-monitor
       nix-tree
     ];
 
     setup.terminal.shellAliases = {
       n = "nix";
-      nhos = "NH_FLAKE=/etc/nixos ${pkgs.nh}/bin/nh os";
-      nhcl = "${pkgs.nh}/bin/nh clean all --keep 3 --nogcroots";
+      nhos = "NH_FLAKE=/etc/nixos ${custom-nh}/bin/nh os";
+      nhcl = "${custom-nh}/bin/nh clean all --keep 3 --nogcroots";
     };
   };
 }
