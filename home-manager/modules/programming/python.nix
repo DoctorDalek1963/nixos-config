ee684f759870cd8e848f3420aedd32beced63cbf
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-07-07 21:23:50 +0100

    Update flake.lock and update probcalc derivation

--- home-manager/modules/programming/python.nix
+++ home-manager/modules/programming/python.nix
@@ -7,10 +7,14 @@
   probcalc = pkgs.python3.pkgs.buildPythonPackage rec {
     pname = "probcalc";
     version = "0.5.0";
+
     src = pkgs.fetchPypi {
       inherit pname version;
       sha256 = "sha256-5k4PXB2DY98/BukWML/o1BX8M8kb0hwHXpBqUeL4AbU=";
     };
+
+    pyproject = true;
+    build-system = [pkgs.python3.pkgs.setuptools];
   };
 
   python =
