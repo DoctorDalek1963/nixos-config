6e6c1294e77e504771e8280dcedb41e0a115ccdc
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-07-18 00:16:41 +0100

    Tell Cinnamon to maximize the window when dragging to top instead of tiling to top half
    Seriousy, who ever wants to tile to top half by default?

--- home-manager/modules/desktopEnvironments/cinnamon/dconf.nix
+++ home-manager/modules/desktopEnvironments/cinnamon/dconf.nix
@@ -5,6 +5,27 @@
   ...
 }: let
   cfg = config.setup.desktopEnvironments;
+
+  background-settings =
+    if cfg.background-slideshow-path != null
+    then {
+      "org/cinnamon/desktop/background/slideshow" = {
+        slideshow-enabled = true;
+        image-source = "directory://${cfg.background-slideshow-path}";
+      };
+    }
+    else {
+      "org/cinnamon/desktop/background" = {
+        picture-uri = "file://${
+          if builtins.isPath cfg.background
+          then cfg.background
+          else cfg.background.light
+        }";
+      };
+      "org/cinnamon/desktop/background/slideshow" = {
+        slideshow-enabled = false;
+      };
+    };
 in {
   config = lib.mkIf osConfig.setup.desktopEnvironments.cinnamon.enable {
     assertions = [
@@ -17,25 +38,11 @@ in {
     dconf = {
       enable = true;
       settings =
-        if cfg.background-slideshow-path != null
-        then {
-          "org/cinnamon/desktop/background/slideshow" = {
-            slideshow-enabled = true;
-            image-source = "directory://${cfg.background-slideshow-path}";
-          };
+        {
+          # Maximize window when dragging to top instead of tiling to top half
+          "org/cinnamon/muffin".tile-maximize = true;
         }
-        else {
-          "org/cinnamon/desktop/background" = {
-            picture-uri = "file://${
-              if builtins.isPath cfg.background
-              then cfg.background
-              else cfg.background.light
-            }";
-          };
-          "org/cinnamon/desktop/background/slideshow" = {
-            slideshow-enabled = false;
-          };
-        };
+        // background-settings;
     };
   };
 }
