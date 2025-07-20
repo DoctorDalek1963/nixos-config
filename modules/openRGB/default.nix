c7d0be4fcb03a238194172a8299c840e0c3b2137
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-07-20 23:25:06 +0100

    Go back to using services.hardware.openrgb

--- modules/openRGB/default.nix
+++ modules/openRGB/default.nix
@@ -5,32 +5,18 @@
   ...
 }: let
   cfg = config.setup.openRGB;
+
   with-all-plugins = import ./with-all-plugins.nix {inherit pkgs;};
+
   package =
-    #
     if cfg.usePlugins
     then with-all-plugins
     else pkgs.openrgb;
 in {
-  # Config taken mostly from
-  # https://github.com/nix-community/nur-combined/blob/4d7247933c41d143d56a01af3da606c1e72eb865/repos/misterio/modules/nixos/openrgb.nix
-  config = lib.mkIf (cfg.enable && cfg.usePlugins) {
-    environment.systemPackages = [package];
-
-    services.udev = {
+  config = lib.mkIf cfg.enable {
+    services.hardware.openrgb = {
       enable = true;
-      packages = [package];
-    };
-
-    boot.kernelModules = ["v4l2loopback" "i2c-dev" "i2c-piix4"];
-
-    systemd.services.openrgb = {
-      description = "OpenRGB Daemon";
-      wantedBy = ["multi-user.target"];
-      serviceConfig = {
-        ExecStart = "${package}/bin/openrgb --server";
-        Restart = "on-failure";
-      };
+      inherit package;
     };
 
     setup.impermanence.keepDirs = ["/var/lib/OpenRGB"];
