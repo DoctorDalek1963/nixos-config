01daf92e602a0549c2b3efc46a288ceec3e26433
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-07-26 00:18:35 +0100

    Re-enable hyprsunset and fix errors

--- home-manager/modules/desktopEnvironments/hyprland/hyprsunset.nix
+++ home-manager/modules/desktopEnvironments/hyprland/hyprsunset.nix
@@ -1,18 +1,15 @@
 {
   pkgs,
   lib,
-  # osConfig,
+  osConfig,
   ...
 }: {
-  config = lib.mkIf false {
-    # osConfig.setup.desktopEnvironments.hyprland.enable {
-
-    # TODO (hyprland 0.51): Re-enable hyprsunset and look into [sunsetr](https://github.com/psi4j/sunsetr)
-    # for sunrise/sunset timing and slow, smooth transitions
-
+  # TODO: Look into [sunsetr](https://github.com/psi4j/sunsetr)
+  # for sunrise/sunset timing and slow, smooth transitions
+  config = lib.mkIf osConfig.setup.desktopEnvironments.hyprland.enable {
     wayland.windowManager.hyprland.settings.exec-once = [(lib.getExe pkgs.hyprsunset)];
 
-    xdg.configFile."hypr/hyprsunset.config".text =
+    xdg.configFile."hypr/hyprsunset.conf".text =
       # hyprlang
       ''
         profile {
@@ -23,13 +20,13 @@
         profile {
             time = 19:00
             temperature = 5000
-            gamma = 85
+            gamma = 0.85
         }
 
         profile {
             time = 22:00
             temperature = 3500
-            gamma = 75
+            gamma = 0.75
         }
       '';
   };
