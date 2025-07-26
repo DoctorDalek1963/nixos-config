f4e38ed7742bb9f3054a84f0613bb1a0c55d8d03
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-07-26 10:54:41 +0100

    Fix sunsetr config
    It requires all keys to be present even though some are unused

--- home-manager/modules/desktopEnvironments/hyprland/sunsetr/default.nix
+++ home-manager/modules/desktopEnvironments/hyprland/sunsetr/default.nix
@@ -35,18 +35,28 @@ in {
 
     xdg.configFile."sunsetr/sunsetr.toml".source =
       (pkgs.writers.writeTOML "sunsetr.toml" {
+        # Sunsetr configuration
         backend = "wayland";
         start_hyprsunset = false;
+
         startup_transition = true;
-        startup_transition_duration = 1;
-        night_temp = 3500;
+        startup_transition_duration = 1; # Seconds
+
         day_temp = 6500;
-        night_gamma = 80;
         day_gamma = 100;
-        update_interval = 60;
+
+        night_temp = 3500;
+        night_gamma = 80;
+
+        update_interval = 60; # Seconds
         transition_mode = "geo";
 
-        # London
+        # Manual transitions, unused but required
+        sunset = "19:00:00";
+        sunrise = "07:00:00";
+        transition_duration = 45; # Minutes
+
+        # Geolocation-based transitions. Location is London
         latitude = 51.508415;
         longitude = -0.125533;
       }).outPath;
