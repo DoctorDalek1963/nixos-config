db0351165185c88464b8b77581fead3fc739e7c1
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-09-14 15:43:11 +0100

    Update flake.lock and remove obsolete nh patch

--- home-manager/modules/programming/nix.nix
+++ home-manager/modules/programming/nix.nix
@@ -4,24 +4,19 @@
   config,
   ...
 }:
-let
-  custom-nh = pkgs.nh.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or [ ]) ++ [ ./nh_clean_show_output.patch ];
-  });
-in
 {
   config = lib.mkIf config.setup.programming.nix {
     home.packages = with pkgs; [
-      custom-nh
       nix-output-monitor
       nix-tree
       nixpkgs-reviewFull
+      nh
     ];
 
     setup.terminal.shellAliases = {
       n = "nix";
-      nhos = "NH_FLAKE=/etc/nixos ${custom-nh}/bin/nh os";
-      nhcl = "${custom-nh}/bin/nh clean all --keep 3 --nogcroots";
+      nhos = "NH_FLAKE=/etc/nixos nh os";
+      nhcl = "nh clean all --keep 3 --nogcroots";
       nrpl = "nix repl --file '<nixpkgs>'";
     };
   };
