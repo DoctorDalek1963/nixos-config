7cc1b21afdd689246fa0a7f7a276e5b2e22f4ea3
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-02-15 13:44:13 +0000

    Remove devtoolbox, add hexyl, and add numbat

--- home-manager/modules/programming/misc.nix
+++ home-manager/modules/programming/misc.nix
@@ -6,6 +6,10 @@
 }:
 {
   config = lib.mkIf config.setup.programming.misc {
-    home.packages = [ pkgs.devtoolbox ];
+    home.packages = [
+      # pkgs.devtoolbox
+      pkgs.hexyl
+      pkgs.numbat
+    ];
   };
 }
