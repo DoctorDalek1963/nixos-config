bd20b0dbf303d1ab7971e878ab17d419f65e739a
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-02-25 21:58:35 +0000

    Don't notify Mealie when Postgres fails to start

--- modules/homeServer/mealie.nix
+++ modules/homeServer/mealie.nix
@@ -33,23 +33,30 @@ in
       };
     };
 
-    systemd.services.mealie.serviceConfig = {
-      DynamicUser = lib.mkForce false;
-      User = "mealie";
+    systemd.services = {
+      mealie.serviceConfig = {
+        DynamicUser = lib.mkForce false;
+        User = "mealie";
 
-      StateDirectory = "mealie";
-      WorkingDirectory = lib.mkForce "/var/lib/mealie";
+        StateDirectory = "mealie";
+        WorkingDirectory = lib.mkForce "/var/lib/mealie";
 
-      TimeoutStartSec = "30 min";
+        TimeoutStartSec = "30 min";
+        RestartSec = "15s";
+        Restart = "always";
 
-      AmbientCapabilities = [
-        "CAP_SETUID"
-        "CAP_SETGID"
-      ];
-      CapabilityBoundingSet = [
-        "CAP_SETUID"
-        "CAP_SETGID"
-      ];
+        AmbientCapabilities = [
+          "CAP_SETUID"
+          "CAP_SETGID"
+        ];
+        CapabilityBoundingSet = [
+          "CAP_SETUID"
+          "CAP_SETGID"
+        ];
+      };
+
+      # Don't notify Mealie when Postgres fails to start
+      postgresql.serviceConfig.RestartMode = "direct";
     };
 
     users = {
