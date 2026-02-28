57b406d83288594b56f69a2045aaa2b4eb6df8b8
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-02-28 18:54:10 +0000

    Make secure boot on Harold automatically provisionable

--- machines/Harold-NixOS/hardware.nix
+++ machines/Harold-NixOS/hardware.nix
@@ -38,6 +38,12 @@
     lanzaboote = {
       enable = true;
       pkiBundle = "/var/lib/sbctl";
+
+      autoGenerateKeys.enable = true;
+      autoEnrollKeys = {
+        enable = true;
+        autoReboot = true;
+      };
     };
 
     loader = {
