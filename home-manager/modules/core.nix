47ea6842eab8462b17839620348972b99bfea209
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-03-09 15:34:45 +0000

    Update home-manager stateVersion to 25.11

--- home-manager/modules/core.nix
+++ home-manager/modules/core.nix
@@ -9,14 +9,7 @@
     inherit (config.setup) username;
     homeDirectory = "/home/${config.setup.username}";
 
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    stateVersion = "23.11"; # Please read the comment before changing.
+    stateVersion = "25.11";
 
     file = {
       ".inputrc".text = ''
