95aa6765c0bd7c323a46ff00847bc0ae508791a1
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-03-26 12:23:49 +0000

    Switch Sasha to secure boot: part 3

--- machines/Sasha-NixOS/hardware.nix
+++ machines/Sasha-NixOS/hardware.nix
@@ -40,6 +40,12 @@
     lanzaboote = {
       enable = true;
       pkiBundle = "/var/lib/sbctl";
+
+      autoGenerateKeys.enable = true;
+      autoEnrollKeys = {
+        enable = true;
+        autoReboot = true;
+      };
     };
 
     loader = {
