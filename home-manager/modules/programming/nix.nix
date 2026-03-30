477f33ec494d290dacc8def6ad90adb85966edc4
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-03-30 09:49:42 +0100

    Add nhos-{boot,switch} aliases

--- home-manager/modules/programming/nix.nix
+++ home-manager/modules/programming/nix.nix
@@ -2,6 +2,7 @@
   pkgs,
   lib,
   config,
+  osConfig,
   ...
 }:
 {
@@ -13,11 +14,25 @@
       nh
     ];
 
-    setup.terminal.shellAliases = {
-      n = "nix";
-      nhos = "NH_FLAKE=/etc/nixos nh os";
-      nhcl = "nh clean all --keep 3 --nogcroots";
-      nrpl = "nix repl --file '<nixpkgs>'";
-    };
+    setup.terminal.shellAliases =
+      let
+        cores =
+          {
+            "Alex-NixOS" = 6;
+            "Bert-NixOS" = 1;
+          }
+          .${osConfig.setup.hostname} or 3;
+
+        nhos = "NH_FLAKE=/etc/nixos nh os";
+      in
+      {
+        n = "nix";
+        inherit nhos;
+        nhcl = "nh clean all --keep 3 --nogcroots";
+        nrpl = "nix repl --file '<nixpkgs>'";
+
+        nhos-boot = "${nhos} boot --ask -- --max-jobs ${toString cores} --keep-going --show-trace --accept-flake-config";
+        nhos-switch = "${nhos} switch --ask -- --max-jobs ${toString cores} --keep-going --show-trace --accept-flake-config";
+      };
   };
 }
