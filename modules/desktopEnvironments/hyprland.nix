5a924f66e2d9927472818b8f8f5ed9b5b57cb447
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-04-13 15:22:10 +0100

    Remove Hyprland non-UWSM session

--- modules/desktopEnvironments/hyprland.nix
+++ modules/desktopEnvironments/hyprland.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -8,6 +9,23 @@
     programs.hyprland = {
       enable = true;
       withUWSM = true;
+
+      package = lib.mkIf config.programs.hyprland.withUWSM (
+        pkgs.hyprland.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.gnused ];
+
+          postInstall =
+            (oldAttrs.postInstall or "")
+            +
+            # bash
+            ''
+              sed -i "s|hyprland.desktop|$out/bin/start-hyprland|" $out/share/wayland-sessions/hyprland-uwsm.desktop
+              rm $out/share/wayland-sessions/hyprland.desktop
+            '';
+
+          passthru.providedSessions = [ "hyprland-uwsm" ];
+        })
+      );
     };
 
     programs.dconf.enable = true;
