6f6dd357a2e6be3e9628700003e4209793988edb
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-04-20 13:16:49 +0100

    Comment out currently unused inputs during migration

--- flake.nix
+++ flake.nix
@@ -6,52 +6,14 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    copyparty = {
-      url = "github:9001/copyparty";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-aspects.url = "github:vic/flake-aspects";
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence.url = "github:nix-community/impermanence";
     import-tree.url = "github:vic/import-tree";
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    lintrans.url = "github:DoctorDalek1963/lintrans";
-    millennium = {
-      url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixvim-config = {
-      url = "github:DoctorDalek1963/nixvim-config";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nur = {
       url = "github:nix-community/nur";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -60,13 +22,5 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    tic-tac-toe.url = "github:DoctorDalek1963/tic-tac-toe";
-    whisper-asr-webservice-flake.url = "github:DoctorDalek1963/whisper-asr-webservice-flake";
-    winter-wonderlights.url = "github:DoctorDalek1963/winter-wonderlights";
-    wordle.url = "github:DoctorDalek1963/wordle";
   };
 }
--- modules/flake/inputs.nix
+++ modules/flake/inputs.nix
@@ -3,59 +3,59 @@
 
   flake-file.inputs = {
     # Hardware and device management
-    impermanence.url = "github:nix-community/impermanence";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Personal projects for home server
-    tic-tac-toe.url = "github:DoctorDalek1963/tic-tac-toe";
-    winter-wonderlights.url = "github:DoctorDalek1963/winter-wonderlights";
-    wordle.url = "github:DoctorDalek1963/wordle";
-
-    # Other home server stuff
-    copyparty = {
-      url = "github:9001/copyparty";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    whisper-asr-webservice-flake.url = "github:DoctorDalek1963/whisper-asr-webservice-flake";
-
-    # home-manager stuff
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    lintrans.url = "github:DoctorDalek1963/lintrans";
-
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixvim-config = {
-      url = "github:DoctorDalek1963/nixvim-config";
-      inputs = {
-        nixpkgs.follows = "nixpkgs"; # Only while nixvim-config uses the same nixpkgs branch
-        flake-parts.follows = "flake-parts";
-      };
-    };
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    millennium = {
-      url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # impermanence.url = "github:nix-community/impermanence";
+    # nixos-hardware.url = "github:NixOS/nixos-hardware";
+    # disko = {
+    #   url = "github:nix-community/disko";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # lanzaboote = {
+    #   url = "github:nix-community/lanzaboote/v1.0.0";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # sops-nix = {
+    #   url = "github:Mic92/sops-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    #
+    # # Personal projects for home server
+    # tic-tac-toe.url = "github:DoctorDalek1963/tic-tac-toe";
+    # winter-wonderlights.url = "github:DoctorDalek1963/winter-wonderlights";
+    # wordle.url = "github:DoctorDalek1963/wordle";
+    #
+    # # Other home server stuff
+    # copyparty = {
+    #   url = "github:9001/copyparty";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # whisper-asr-webservice-flake.url = "github:DoctorDalek1963/whisper-asr-webservice-flake";
+    #
+    # # home-manager stuff
+    # home-manager = {
+    #   url = "github:nix-community/home-manager/master";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    #
+    # lintrans.url = "github:DoctorDalek1963/lintrans";
+    #
+    # nix-index-database = {
+    #   url = "github:nix-community/nix-index-database";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # nixvim-config = {
+    #   url = "github:DoctorDalek1963/nixvim-config";
+    #   inputs = {
+    #     nixpkgs.follows = "nixpkgs"; # Only while nixvim-config uses the same nixpkgs branch
+    #     flake-parts.follows = "flake-parts";
+    #   };
+    # };
+    # noctalia = {
+    #   url = "github:noctalia-dev/noctalia-shell";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # millennium = {
+    #   url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 }
