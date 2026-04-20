4f6d6d094bb880fbec21cf4181a1578872eb1e12
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-04-20 01:56:24 +0100

    Remove unnecessary import-tree input

--- modules/flake/dendritic.nix
+++ modules/flake/dendritic.nix
@@ -7,6 +7,5 @@
   flake-file.inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-file.url = "github:vic/flake-file";
-    import-tree.url = "github:vic/import-tree";
   };
 }
