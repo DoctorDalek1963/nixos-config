1a20406143e640a2eda23528e801c15760b6253d
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-05-01 16:32:50 +0100

    Strip programming.haskell to only include ~/.ghci

--- home-manager/modules/programming/haskell.nix
+++ home-manager/modules/programming/haskell.nix
@@ -1,23 +1,13 @@
 {
-  pkgs,
   lib,
   config,
   ...
 }:
 {
   config = lib.mkIf config.setup.programming.haskell {
-    home = {
-      packages = [ pkgs.ghc ];
-
-      file.".ghci".text =
-        # haskell
-        ''
-          :set prompt "\ESC[1;35m\STXλ> \ESC[m\STX"
-          :set editor "${config.setup.shared.nvim.path}"
-          naturals = [1..]
-          sieve (p : xs) = p : sieve [ x | x <- xs, x `mod` p > 0 ]
-          primes = 2 : sieve [3,5..]
-        '';
-    };
+    home.file.".ghci".text = ''
+      :set prompt "\ESC[1;35m\STXλ> \ESC[m\STX"
+      :set editor "${config.setup.shared.nvim.path}"
+    '';
   };
 }
